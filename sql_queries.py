e32b4f1c4006b72532c575102f8bdb7c07091da8
mohamedbakhet <mohamed33bakhet@gmail.com>
2022-08-23 20:15:43 +0200

    edit on sql_queries file

--- sql_queries.py
+++ sql_queries.py
@@ -8,96 +8,96 @@ time_table_drop = "drop table IF EXISTS time;"
 
 # CREATE TABLES
 
-songplay_table_create = ("create table if not exists songplays (\
-  songplay_id serial primary key, \
-  start_time TIMESTAMP NOT NULL, \
-  user_id int NOT NULL, \
-  level varchar(5), \
-  song_id varchar, \
-  artist_id varchar,\ 
-  session_id varchar NOT NULL,\ 
-  location varchar, \
-  user_agent varchar\
-);")
+songplay_table_create = ("""create table if not exists songplays (\
+  songplay_id serial primary key, 
+  start_time TIMESTAMP NOT NULL, 
+  user_id int NOT NULL, 
+  level varchar(5), 
+  song_id varchar, 
+  artist_id varchar, 
+  session_id varchar NOT NULL,
+  location varchar, 
+  user_agent varchar
+);""")
 
-user_table_create = ("create table if not exists users (\
-  user_id int primary key, \
-  first_name varchar, \
-  last_name varchar, \
-  gender char, \
-  level varchar(5)\
-);")
+user_table_create = ("""create table if not exists users (
+  user_id int primary key, 
+  first_name varchar, 
+  last_name varchar, 
+  gender char, 
+  level varchar(5)
+);""")
 
-song_table_create = ("create table if not exists songs (\
-  song_id varchar primary key, title varchar NOT NULL, \
-  artist_id varchar NOT NULL, year int, \
-  duration float NOT NULL\
-);")
+song_table_create = ("""create table if not exists songs (
+  song_id varchar primary key, title varchar NOT NULL,
+  artist_id varchar NOT NULL, year int, 
+  duration float NOT NULL
+);""")
 
-artist_table_create = ("create table if not exists artists (\
-    artist_id  varchar primary key,\
-    name varchar NOT NULL,\
-    location varchar,\
-    latitude float,\
-    longitude float\
-);")
+artist_table_create = ("""create table if not exists artists (
+    artist_id  varchar primary key,
+    name varchar NOT NULL,
+    location varchar,
+    latitude float,
+    longitude float
+);""")
 
-time_table_create = ("create table if not exists time (\
-  start_time TIMESTAMP PRIMARY KEY,\
-  hour int, \
-  day int,\
-  week int,\
-  month int,\
-  year int, \
-  weekday varchar\
-);")
+time_table_create = ("""create table if not exists time (
+  start_time TIMESTAMP PRIMARY KEY,
+  hour int, 
+  day int,
+  week int,
+  month int,
+  year int, 
+  weekday varchar
+);""")
 
 # INSERT RECORDS
 
-songplay_table_insert = ("insert into songplays(\
-    start_time, user_id, level, song_id, artist_id , \
-    session_id, location, user_agent)\ 
-    values\
-    (%s,%s,%s,%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING;")
+songplay_table_insert = ("""insert into songplays(
+    start_time, user_id, level, song_id, artist_id , 
+    session_id, location, user_agent)
+    values
+    (%s,%s,%s,%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING;""")
 
-user_table_insert = ("insert into users (\
-      user_id, first_name, last_name, gender,level \
-    ) \
-    values\
-    (% s,% s,% s,% s,% s) ON CONFLICT (user_id) DO NOTHING;")
+user_table_insert = ("""insert into users (
+      user_id, first_name, last_name, gender,level 
+    ) 
+    values
+    (%s,%s,%s,%s,%s) ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level;""")
 
-song_table_insert = ("insert into songs (\
-      song_id, title, artist_id, year, duration\
-    ) \
-    values\ 
-      (% s,% s,% s,% s,% s) ON CONFLICT (song_id) DO NOTHING;")
+song_table_insert = ("""insert into songs (
+      song_id, title, artist_id, year, duration
+    ) 
+    values 
+      (%s,%s,%s,%s,%s) ON CONFLICT (song_id) DO NOTHING;""")
 
-artist_table_insert = ("insert into artists (\
-      artist_id, name, location, latitude, \
-      longitude\
-    ) \
-    values\ 
-      (% s,% s,% s,% s,% s) ON CONFLICT (artist_id) DO NOTHING;")
+artist_table_insert = ("""insert into artists (
+      artist_id, name, location, latitude, 
+      longitude
+    ) 
+    values 
+      (%s,%s,%s,%s,%s) ON CONFLICT (artist_id) DO NOTHING;""")
 
 
-time_table_insert = ("insert into time (\
-      start_time, hour, day, week, month, \
-      year, weekday\
-    ) \
-    values \
-      (% s,% s,% s,% s,% s,% s,% s) ON CONFLICT (start_time) DO NOTHING;")
+time_table_insert = ("""insert into time (
+      start_time, hour, day, week, month, 
+      year, weekday
+    ) 
+    values 
+      (%s,%s,%s,%s,%s,%s,%s) ON CONFLICT (start_time) DO NOTHING;""")
 
 # FIND SONGS
-song_select = ("select \
-  songs.song_id, \
-  songs.artist_id\ 
-from \
-  songs \
-  join artists on songs.artist_id = artists.artist_id \
-where \
-  songs.title =% s\ 
-  and artists.name =% s\ 
-  and songs.duration =% s;")
+song_select = ("""select 
+  songs.song_id, 
+  songs.artist_id
+from 
+  songs 
+  join artists on songs.artist_id = artists.artist_id 
+where 
+  songs.title =%s
+  and artists.name =%s 
+  and songs.duration =%s;""")
 
 # QUERY LISTS
 
